e1175d389601259b51502dd0c43d4c64936c3765
modisuraj360 <modisuraj360@gmail.com>
2023-08-15 19:50:51 +0530

    Add files via upload

---
+++ src/online/survey/system/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package online.survey.system;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Rating();
+    }
+}
